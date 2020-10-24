b33e4ccf07d4e64b8ef1e00360a4ce1cd60578ff
Jakub Prymak <jprymak@gmail.com>
2020-10-24 22:26:28 +0200

    refactor: create Month instances

---
+++ months.js
@@ -0,0 +1,32 @@
+import { Month } from "./Month.js";
+
+const january = new Month(0);
+const february = new Month(1);
+const march = new Month(2);
+const april = new Month(3);
+const may = new Month(4);
+const june = new Month(5);
+const july = new Month(6);
+const august = new Month(7);
+const september = new Month(8);
+const october = new Month(9);
+const november = new Month(10);
+const december = new Month(11);
+
+const months = [];
+months.push(
+  january,
+  february,
+  march,
+  april,
+  may,
+  june,
+  july,
+  august,
+  september,
+  october,
+  november,
+  december
+);
+
+export default months;
