132aa15e489294295074ec42985722f5dc970c4b
Jakub Prymak <jprymak@gmail.com>
2020-10-25 12:39:28 +0100

    feat: add habit status counting

--- index.js
+++ index.js
@@ -1,22 +1,26 @@
 // Import stylesheets
 import "./style.css";
-import Month from "./months.js";
-// import { dates, january } from "./dates";
-
+import { currentDate } from "./Month.js";
+import months from "./months.js";
 // Write Javascript code!
+const currentMonth = currentDate.getMonth();
 
 const calendar = document.querySelector(".calendar");
 const nextButton = document.querySelector(".nextBtn");
 const previousButton = document.querySelector(".previousBtn");
 
 for (let i = 0; i <= 11; i++) {
-  calendar.appendChild(new Month(i).createContent());
+  calendar.appendChild(months[i].createContent());
 }
 
-const calendarMonths = document.querySelectorAll(".month");
+const calendarMonths = Array.from(document.querySelectorAll(".month"));
 
 calendarMonths.forEach((month, index) => {
-  if (index === 0 || index === 1 || index === 2) {
+  if (
+    index === currentMonth - 1 ||
+    index === currentMonth ||
+    index === currentMonth + 1
+  ) {
     month.classList.add("month--displayed");
   }
 });
@@ -25,6 +29,37 @@ nextButton.addEventListener("click", displayNextMonth);
 previousButton.addEventListener("click", displayPreviousMonth);
 
 let visibleMonths = document.querySelectorAll(".month--displayed");
+const habitStatus = document.querySelectorAll(".habit-status");
+
+habitStatus.forEach((status) =>
+  status.addEventListener("click", (e) => {
+    const monthToUpdate = e.currentTarget.closest(".month");
+    const habitsStateToUpdate = monthToUpdate.querySelector(".month__habits");
+    const currentDay = e.currentTarget.closest(".day");
+    const monthIndex = calendarMonths.indexOf(monthToUpdate);
+    const daysList = Array.from(monthToUpdate.querySelectorAll(".day"));
+    const dayIndex = daysList.indexOf(currentDay);
+
+    if (e.currentTarget.classList.contains("habit-status--broken")) {
+      e.currentTarget.classList.remove("habit-status--broken");
+      e.currentTarget.classList.add("habit-status--maintained");
+      months[monthIndex].days[dayIndex].maintained = true;
+    } else {
+      e.currentTarget.classList.remove("habit-status--maintained");
+      e.currentTarget.classList.add("habit-status--broken");
+      months[monthIndex].days[dayIndex].maintained = false;
+    }
+    const habitMaintainedCount = months[monthIndex].days.reduce((maintainedCount, day) => {
+      if (day.maintained !== false) {
+        maintainedCount += 1;
+      }
+
+      return maintainedCount;
+    }, 0);
+
+    habitsStateToUpdate.innerHTML = `${habitMaintainedCount}/${months[monthIndex].daysLength}`;
+  })
+);
 
 function displayNextMonth() {
   if (visibleMonths[2].querySelector(".month__name").innerHTML === "Grudzień")
